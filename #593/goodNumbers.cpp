d330311596d57bcff257079e9f4b45e81b82448d
Mano Sriram <mano.sriram0@gmail.com>
2019-10-24 01:00:32 +0530

    Complete goodNumbers.

--- #593/goodNumbers.cpp
+++ #593/goodNumbers.cpp
@@ -2,40 +2,49 @@
 #include <iostream>
 using namespace std;
 
+bool isPower(int x) {
+    while (x%3 == 0)  x = x / 3;
+
+    return x == 1;
+}
+
 int getNearestPower3(int n) {
     if (n == 1) return 0;
-    int x = 1;
-    while (pow(3, x) <= n) {
+
+    int x = 0;
+    while (pow(3, x) < n) {
         ++x;
     }
     return --x;
 }
 
-int getGoodNumber(int n) {
+bool getGoodNumber(int n) {
+    if (isPower(n))
+        return true;
+
     int temp = n;
     
-    while (true) {
+    while (temp != 1) {
         temp = pow(3, getNearestPower3(temp));
-
-        cout << "Temp: " << temp << " " << "n: " << n << endl;
+        
+        // cout << "Temp: " << temp << " " << "n: " << n << endl;
         if (n - temp >= 0) n -= temp;
 
         if (n == 0) return true;
-
-        if (temp == 1 && n != 0) return false;
     }
+    return false;
 }
 
 int main() {
-    int n, T;
+    int T;
     cin >> T;
     while (T--) {
+        int n;
         cin >> n;
         if (n == 1)
             {cout << 1 << '\n'; continue;}
-        // cout << getNearestPower3(n);
+    
         while (!getGoodNumber(n)) ++n;
-
         cout << n << '\n';
     }
 }
